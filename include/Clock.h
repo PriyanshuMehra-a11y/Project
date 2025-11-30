281ec89dda28314f06695daaaa2253170f5cb48a
PriyanshuMehra-a11y <mehramillie4@gmail.com>
2025-11-30 17:37:21 +0530

    Update Clock.h

--- include/Clock.h
+++ include/Clock.h
@@ -1,1 +1,12 @@
+#ifndef DIGITAL_CLOCK_H
+#define DIGITAL_CLOCK_H
 
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>   
+#include <stdlib.h>  
+// Function Prototypes
+void displayClock();   
+void runClock();       
+
+#endif
